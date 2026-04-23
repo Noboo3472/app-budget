336ae51742224128820ce5320e7e581382c1e647
User <Noboo>
2026-04-23 14:38:03 +0200

    feat route GET/POST/DELETE/api/shopping_list_items

---
+++ src/app/api/shopping_list_items/route.js
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server'
+import prisma from '@/lib/prisma'
+
+export async function GET() {
+  const shoppingListItems = await prisma.shoppingListsItems.findMany()
+  return NextResponse.json(shoppingListItemss)
+}
+
+export async function POST(request) {
+  const body = await request.json()
+  
+  const shoppingListItems = await prisma.shoppingListsItems.create({
+    data: {
+      libelle: body.libelle,
+      montant: body.montant,
+      quantite : body.quantite,
+      cocher: body.cocher,
+      shoppingListsID: body.shoppingListsID
+    }
+  })
+  
+  return NextResponse.json(shoppingListItems)
+}
